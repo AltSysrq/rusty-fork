57cdb02b4b77d0208aa7278cd5c53de24bbf155c
Jason Lingle <jason@lin.gl>
2018-06-02 09:33:11 -0600

    Update version referenced in the readme.

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ In `Cargo.toml`, add
 
 ```toml
 [dev-dependencies]
-rusty-fork = "0.1.0"
+rusty-fork = "0.2.0"
 ```
 
 and to your crate root add
--- src/lib.rs
+++ src/lib.rs
@@ -44,7 +44,7 @@
 //!
 //! ```toml
 //! [dev-dependencies]
-//! rusty-fork = "0.1.0"
+//! rusty-fork = "0.2.0"
 //! ```
 //!
 //! and to your crate root add
